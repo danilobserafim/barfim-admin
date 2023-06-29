1e877d83a5f737890d116a66a6e88cd7afe17cb7
Danilo Serafim <danilob.serafim@gmail.com>
2023-06-29 13:36:48 -0300

    terminando a noite

--- src/app/services/loja.service.ts
+++ src/app/services/loja.service.ts
@@ -10,21 +10,27 @@ import { base_URL } from "../../../enviroment";
 })
 export class LojaService {
   data:any;
+
   constructor(private http: HttpClient ) { }
+
   getPordutos():Observable<any>{
     return this.http.get(`${base_URL}/produtos`)
   }
+
   getCategorias():Observable<any>{
     return this.http.get(`${base_URL}/categorias`)
   }
+
   postPorduto(produto: {nome: string, descricao: string, valor: number, categoria: number, quantidade: number}):Observable<any>{
     let options = {headers: {"content-type": "application/json; charset=utf-8", "access-control-allow-origin":"*"}}
     let body = { nome: produto.nome, descricao: produto.descricao, categoria: produto.categoria, valor: produto.valor, quantidade: produto.quantidade }
     return this.http.post<any>(`${base_URL}/produtos/`, body, options )
   }
+
   postVenda(venda: {cliente_id: number, valor_venda: number, desconto: number, total: number, carrinho: []}):Observable<any>{
     let options = {headers: {"content-type": "application/json; charset=utf-8", "access-control-allow-origin":"*"}}
     let body = { cliente_id: venda.cliente_id, valor_venda: venda.valor_venda, desconto: venda.desconto, total: venda.total, carrinho: venda.carrinho }
     return this.http.post<any>(`${base_URL}/vendas/`, body, options )
   }
+  
 }
